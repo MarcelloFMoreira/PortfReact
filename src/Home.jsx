247dbd40f0fa3877ce68bf4ae64340a030015e5d
Marcello de Freitas Moreira <marcellofreitas1227@gmail.com>
2025-07-28 19:00:08 -0300

    hover com estilizacao inicial

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,22 @@
 import Perfil from "./assets/FotoPerfil.png"
+import iconDownload from "./assets/iconDownload.png"
+import "./home.css"
 
 
 function Home() {
     return (
             <>
+            <div className="ContainerTotal">
                 <div className="ContainerEsquerda">               
                     <h1>Olá! eu sou o Marcello,</h1>
                     <h2>Desenvolvedor full stack</h2>
-                    <button>Projetos</button>
-                    <button>Dowload CV</button>
+                    <button id="bt1">Projetos</button>
+                    <button id="bt2"> <img src={iconDownload}/>Dowload CV</button>
                 </div>
                 <div className="ContainerDireita">
                     <img src={Perfil}  />
                 </div>
+            </div>
                 
             </>
     )
